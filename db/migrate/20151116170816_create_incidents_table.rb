77821bd2dcc61cc93b89728960641d9c4987689d
Nick Campion <nick.campion@fasthosts.com>
2015-11-17 14:05:19 +0000

    todo list added

--- db/migrate/20151116170816_create_incidents_table.rb
+++ db/migrate/20151116170816_create_incidents_table.rb
@@ -1,5 +1,16 @@
 class CreateIncidentsTable < ActiveRecord::Migration
   def change
+    # TODO: 
+    # Title
+    # Source
+    # Incident Manager => from Users table
+    # Technical Owner  => from Users table
+    # Priority         => Array from settings table
+    # Impact           => Free text or from Asset DB somehow
+    # Platform         => Array list for now - hopefull from assets table
+    # CS Status        => boolean to Open / Closed
+    # Technical Status => boolean Open / Closed
+    # Update Due       => time.now + <%= Setting.priority.update_frequency %>.hour
     create_table :incidents_tables do |t|
     end
   end
