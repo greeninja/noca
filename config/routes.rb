d4b4d04396a1b5e2545ca55b69c49560360314ea
Greeninja <nick@greeninja.co.uk>
2015-11-17 22:28:31 +0000

    add Incidents controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'incidents/index'
+
+  get 'incidents/show'
+
+  get 'incidents/new'
+
+  get 'incidents/edit'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
