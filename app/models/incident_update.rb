785840ec7e1027036f48ba1370b52ea91188c8a7
Greeninja <nick@greeninja.co.uk>
2015-11-17 22:26:14 +0000

    model for incident updates with lambda

--- app/models/incident_update.rb
+++ app/models/incident_update.rb
@@ -1,2 +1,7 @@
 class IncidentUpdate < ActiveRecord::Base
+  belongs_to :incident
+  
+  # Scopes
+  scope :csupdate, lambda { where("cs_update IS NOT NULL").order("created_at DESC") }
+  scope :techupdate, lambda { where("tech_update IS NOT NULL").order("created_at DESC") }
 end
