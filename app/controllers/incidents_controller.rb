0b6af1d392a39512317c8e1ad85fe734f2e60521
Greeninja <nick@greeninja.co.uk>
2015-11-17 22:37:47 +0000

    simple incident controller

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -1,13 +1,38 @@
 class IncidentsController < ApplicationController
   def index
+    @incidents = Incident.opensorted
   end
 
   def show
+    @incident = Incident.find(params[:id])
   end
 
   def new
+    @incident = Incident.new
+  end
+
+  def create
+    @incident = Incident.new(incident_params)
+    if @incident.save
+      # If the save is successful flash a message on return to index
+      flash[:notice] = "New Incident Created = '#{@incident.title}'"
+      redirect_to(:action => 'index')
+    else
+      render('new')
+    end
   end
 
   def edit
+    @incident = Incident.find(params[:id])
+  end
+
+  def update
+    @incident = Incident.find(params[:id])
+    if @incident.update_attributes(incident_params)
+      flash[:notice] = "Incident '#{@incident.title} successfully updated"
+      redirect_to(:action => 'show', :id => @incident.id)
+    else
+      render('edit')
+    end
   end
 end
