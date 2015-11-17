bb2aab8e1d3f36654d3059fa422c034a2c3b43bf
Greeninja <nick@greeninja.co.uk>
2015-11-17 21:25:21 +0000

    updates table added

--- db/migrate/20151117212100_create_incident_updates.rb
+++ db/migrate/20151117212100_create_incident_updates.rb
@@ -1,6 +1,11 @@
 class CreateIncidentUpdates < ActiveRecord::Migration
   def change
     create_table :incident_updates do |t|
+      t.integer "incident_id"
+      t.text "cs_update"
+      t.text "tech_update"
+      t.timestamps null: false
     end
+    add_index("incident_updates", "incident_id")
   end
 end
