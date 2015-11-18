58c723125751e3f0f0d7d001a336fe9dc3a85747
Greeninja <nick@greeninja.co.uk>
2015-11-18 22:34:49 +0000

    add index and private method

--- app/controllers/incident_updates_controller.rb
+++ app/controllers/incident_updates_controller.rb
@@ -1,5 +1,10 @@
 class IncidentUpdatesController < ApplicationController
+
+  before_action :find_incident
+
   def index
+    @updates = IncidentUpdate.where(:incident_id => @incident.id).sorted
+    #@updates = IncidentUpdate.where(:incident_id => @incident.id).sorted
   end
 
   def show
@@ -10,4 +15,12 @@ class IncidentUpdatesController < ApplicationController
 
   def edit
   end
+
+private
+  def find_incident
+    if params[:id]
+      @incident = Incident.find(params[:id])
+    end
+  end
+
 end
