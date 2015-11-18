56cbe3bf07096015b2f5942debed1105935a0859
Greeninja <nick@greeninja.co.uk>
2015-11-18 22:35:27 +0000

    add sorted lambda

--- app/models/incident_update.rb
+++ app/models/incident_update.rb
@@ -2,7 +2,7 @@ class IncidentUpdate < ActiveRecord::Base
   belongs_to :incident
   
   # Scopes
-  scope :sorted, lambda { order("incident_update.updated_at DESC") }
+  scope :sorted, lambda { order("updated_at DESC") }
   scope :csupdate, lambda { where("cs_update IS NOT NULL").order("created_at DESC") }
   scope :techupdate, lambda { where("tech_update IS NOT NULL").order("created_at DESC") }
 end
